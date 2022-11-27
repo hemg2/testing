9a312135e21f27aabe8171ea3637ad675069b4cb
hemg2 <hemg2@naver.com>
2022-11-27 21:20:55 +0900

    11.27 codingTest 10개

---
+++ CodingTest/11월5주차 시작/11.27/11.27.swift
@@ -0,0 +1,91 @@
+//
+//  11.27.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/11/27.
+//
+
+import Foundation
+/*
+ 각도기
+ func solution(_ angle:Int) -> Int {
+     if angle <= 89 {
+         return 1
+     } else if angle == 90 {
+         return 2
+     }
+     else if angle <= 179 {
+         return 3
+     } else if angle == 180 {
+         return 4
+     }
+     return 0
+ }
+ 
+ 
+ 두 수의 나눗셈
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     var aa = Double(num1)
+     var bb = Double(num2)
+     var a = aa / bb
+     var b = a * 1000
+     
+     return Int(b)
+ }
+
+ 짝수의 합
+ func solution(_ n:Int) -> Int {
+     var a : Int = 0
+     for i in 0...n {
+         if i % 2 == 0 {
+             a = a+i
+         }
+     }
+     return a
+ }
+
+ 
+ 
+두 수의 차
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     return num1 - num2
+ }
+ 
+ 숫자 비교하기
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     if num1 == num2 {
+         return 1
+     } else {
+         return -1
+     }
+     return 0
+ }
+ 
+ 몫 구하기
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     return num1 / num2
+ }
+ 
+ 두 수의 곱
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     return num1 * num2
+ }
+ 
+ 나머지 구하기
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     var a = num1 % num2
+     return a
+ }
+ 
+ 나이 출력
+ func solution(_ age:Int) -> Int {
+     return 2023 - age
+ }
+ 
+ 두 수의 합
+ func solution(_ num1:Int, _ num2:Int) -> Int {
+     return num1 + num2
+ }
+ 
+ 
+ */
