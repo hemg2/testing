6da26f52a103d009fabdb4dfe4cabf9ee3bf050e
hemg2 <hemg2@naver.com>
2022-11-29 00:22:17 +0900

    11.28 codingTest

---
+++ CodingTest/11월5주차 시작/11.27/11.28.swift
@@ -0,0 +1,42 @@
+//
+//  11.28.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/11/29.
+//
+
+import Foundation
+/*
+ 
+ 
+ 배열의 평균 값
+ 
+ func solution(_ numbers:[Int]) -> Double {
+     var a = 0
+     for i in numbers {
+         a += i
+     }
+     var j = Double(numbers.count)  // 카운트니깐 갯수
+     var g = Double(a) / j  // i한테 다줬으니깐 나누기
+ 
+     return g
+ }
+ print(solution([1,2,3,4,5,6,7,8,9,10]))
+ 
+
+
+
+ 양꼬치
+ 
+ 
+ func solution(_ n:Int, _ k:Int) -> Int {
+     var i = 12000 * n
+     var j = (k-Int(n/10))*2000
+     return i + j
+ }
+ 
+ print(solution(64, 6))
+
+ 
+ 
+ */
