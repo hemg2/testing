dc79600e13695eb5cd7074c181cf15fdd202887c
hemg2 <hemg2@naver.com>
2022-11-30 00:16:10 +0900

    11.29 codingTest

---
+++ CodingTest/11월5주차 시작/11.27/11.29.swift
@@ -0,0 +1,44 @@
+//
+//  11.29.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/11/30.
+//
+
+import Foundation
+
+////배열 뒤집기
+//func solution(_ num_list:[Int]) -> [Int] {
+//    return num_list.reversed()
+//}
+//print(solution([1, 2, 3, 4, 5]))
+
+
+//  머쓱이보다 키큰사람
+//func solution(_ array:[Int], _ height:Int) -> Int {
+//    var arr: [Int] = []
+//
+//    for i in array {
+//        if i > height {
+//            arr.append(i)
+//        }
+//    }
+//
+//    return arr.count
+//}
+//print(solution([149, 180, 192, 170], 167))
+
+
+// 중복된 숫자 갯수
+//func solution(_ array:[Int], _ n:Int) -> Int {
+//    var a : [Int] = []
+//
+//    for i in array {
+//        if i == n {
+//            a.append(i)
+//
+//        }
+//    }
+//    return a.count
+//}
+//print(solution([1,1,2,3,4,5], 1))
