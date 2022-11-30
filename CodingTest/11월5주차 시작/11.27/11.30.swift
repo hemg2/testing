9a00b3f8fadc6e8b3ebad5792ec5a38e7cb1c97f
hemg2 <hemg2@naver.com>
2022-11-30 23:01:47 +0900

    11.30 codingTest

---
+++ CodingTest/11월5주차 시작/11.27/11.30.swift
@@ -0,0 +1,28 @@
+//
+//  11.30.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/11/30.
+//
+
+import Foundation
+
+
+//배열 두배 만들기
+//func solution(_ numbers:[Int]) -> [Int] {
+//    var a : [Int] = []
+//
+//    for i in numbers {
+//        a.append(i * 2)
+//    }
+//
+//    return Array(a)
+//}
+//print(solution([1, 2, 3, 4, 5]))
+
+
+//문자열 뒤집기
+//func solution(_ my_string:String) -> String {
+//    return String(my_string.reversed())
+//}
+//print(solution("one"))
