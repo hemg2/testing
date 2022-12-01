2bb0d21be639840548560397037670373735115f
hemg2 <hemg2@naver.com>
2022-12-01 21:41:05 +0900

    12.1 codingTest

---
+++ CodingTest/11월5주차 시작/11.27/12.1.swift
@@ -0,0 +1,33 @@
+//
+//  12.1.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/12/01.
+//
+
+import Foundation
+
+
+//배열 자르기
+//func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+//    var a = [Int]()
+//
+//    for i in numbers {
+//        a.append(i)
+//        print(a)
+//    }
+//    var v = a[num1...num2]
+//    return Array(v)
+//}
+//print(solution([1, 2, 3, 4, 5], 1, 3))
+
+
+//아메리카노 할인
+//func solution(_ money:Int) -> [Int] {
+//    var a : [Int] = []
+//    var b = money / 5500
+//
+//    a.append(b)
+//    a.append(money - (5500 * b))
+//    return a
+//}
