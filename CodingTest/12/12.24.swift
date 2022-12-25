6107dcbcf216d303c900f2e21b414839fd9d3c9d
hemg2 <hemg2@naver.com>
2022-12-25 13:18:41 +0900

    test

---
+++ CodingTest/12/12.24.swift
@@ -0,0 +1,41 @@
+//
+//  12.24.swift
+//  CodingTest
+//
+//  Created by 1 on 2022/12/24.
+//
+
+import Foundation
+
+
+//func solution(_ num_list:[Int]) -> [Int] {
+//    var a : [Int] = []
+//    var b : [Int] = []
+//    var c : [Int] = []
+//
+//    for i in num_list {
+//        if i % 2 == 0 {
+//            a.append(i)
+//            print("a값\(a)")
+//        } else {
+//            b.append(i)
+//            print("b값\(b)")
+//        }
+//    }
+//    print("밖값\(a.count)")
+//    print("밖값\(b.count)")
+//    c.append(a.count)
+//    c.append(b.count)
+//    return c
+//}
+//print(solution([1, 2, 3, 4, 5]))
+
+
+
+// 12.24
+//func solution(_ message:String) -> Int {
+//    var a = String(repeating: message, count: 2)
+//    print("a의 값\(a)")
+//    return a.count
+//}
+//print(solution("i love you~"))
