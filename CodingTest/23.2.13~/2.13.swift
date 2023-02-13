da7eaf9a17a97bff030864b2f31b6e4b139bca07
hemg2 <hemg2@naver.com>
2023-02-14 00:58:17 +0900

    2.13

---
+++ CodingTest/23.2.13~/2.13.swift
@@ -0,0 +1,38 @@
+//
+//  2.13.swift
+//  CodingTest
+//
+//  Created by 1 on 2023/02/14.
+//
+
+import Foundation
+
+/*
+  음양 더하기
+ func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+     var a : [Int] = []
+     var b : Int = 0
+     
+
+     for j in 0..<absolutes.count {
+         if signs[j] == true {
+             a.append(absolutes[j])
+         } else {
+             a.append(-absolutes[j])
+         }
+     }
+     
+     for i in a {
+         b += i
+     }
+    
+     return b
+ }
+ print(solution([4,7,12], [true, false, true]))
+ print(solution([4,7,12], [false, true, false]))
+ print(solution([1,2,3], [false, false, true]))
+ print(solution([1,2,3,5], [false, false, true, false]))
+ 
+ 
+ 
+ */
