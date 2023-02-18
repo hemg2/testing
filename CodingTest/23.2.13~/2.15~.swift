6e8ea81850eeeb1d6be8324060f17dc4810f9614
hemg2 <hemg2@naver.com>
2023-02-18 17:08:42 +0900

    2.17

--- CodingTest/23.2.13~/2.15~.swift
+++ CodingTest/23.2.13~/2.15~.swift
@@ -11,6 +11,29 @@ import Foundation
 /*
  
  2.17
+ 
+ 
+ // 최대공약수와 최소공배수
+ func solution(_ n:Int, _ m:Int) -> [Int] {
+     return [gcd(n, m), lcm(n, m)]
+ }
+
+ //최대 공약수
+ func gcd(_ a: Int, _ b: Int) -> Int {
+     if b == 0 {
+         return a
+     } else {
+         return gcd(b, a % b)
+     }
+ }
+ // 공배수
+ func lcm(_ a: Int, _ b: Int) -> Int {
+     return a * b / gcd(a, b)
+ }
+ print(solution(3, 12))
+ print(solution(2, 5))
+ 
+ 
  약수의 개수와 덧셈
  
  func solution(_ left:Int, _ right:Int) -> Int {
