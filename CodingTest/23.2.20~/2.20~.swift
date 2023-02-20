ff55e6aa1177c92f46a386d60d2f74f920c0f9dd
hemg2 <hemg2@naver.com>
2023-02-20 22:56:44 +0900

    2.20(1)

--- CodingTest/23.2.20~/2.20~.swift
+++ CodingTest/23.2.20~/2.20~.swift
@@ -10,6 +10,30 @@ import Foundation
 
 /*
  
+ 
+ // 부족한 금액 계산하기
+ //func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
+ //    var answer:Int64 = Int64(money)
+ //    var a = price
+ //    var b:Int64 = 0
+ //
+ //    for i in 1...count {
+ //        b += Int64(i * a)
+ ////        print(i)
+ //    }
+ ////    print(b)
+ //    var c = b - answer
+ //
+ //    if b < answer {
+ //        return 0
+ //    }
+ //    print("c = \(c)")
+ //    return c
+ //}
+ //print(solution(3, 20, 4))
+
+ 
+ 
  // 문자열 다루기 기본
  //func solution(_ s:String) -> Bool {
  //    if s.count == 4 || s.count == 6 {
