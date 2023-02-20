1196a1f25d42350986a74fb76fefc6494a057231
hemg2 <hemg2@naver.com>
2023-02-20 18:07:19 +0900

    2.20

---
+++ CodingTest/23.2.20~/2.20~.swift
@@ -0,0 +1,27 @@
+//
+//  2.20~.swift
+//  CodingTest
+//
+//  Created by 1 on 2023/02/20.
+//
+
+import Foundation
+
+
+/*
+ 
+ // 문자열 다루기 기본
+ //func solution(_ s:String) -> Bool {
+ //    if s.count == 4 || s.count == 6 {
+ //        if Int(s) != nil {
+ //            return true
+ //        } else {
+ //            return false
+ //        }
+ //    }
+ //    return false
+ //}
+ //print(solution("a1234"))
+ //print(solution("1234"))
+
+ */
