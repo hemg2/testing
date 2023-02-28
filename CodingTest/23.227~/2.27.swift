6f702be80d52946e9e575dd1352414154323678c
hemg2 <hemg2@naver.com>
2023-02-28 14:33:42 +0900

    2.28

---
+++ CodingTest/23.227~/2.27.swift
@@ -0,0 +1,35 @@
+//
+//  2.27.swift
+//  CodingTest
+//
+//  Created by 1 on 2023/02/28.
+//
+
+import Foundation
+
+
+/*
+ 
+ 
+ // 두개 뽑아서 더하기
+ func solution(_ numbers:[Int]) -> [Int] {
+     var a: Set<Int> = []
+     var b = Int()
+     
+     for i in 0..<numbers.count-1 {
+         for j in 0..<numbers.count {
+             if i == j {
+                 
+             } else {
+                 b = numbers[i] + numbers[j]
+                 a.insert(b)
+             }
+         }
+
+     }
+     return Array(a.sorted())
+ }
+ print(solution([2, 1, 3, 4, 1]))
+
+ 
+ */
