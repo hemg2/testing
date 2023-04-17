15bc52be40bd50ff1625777486fef8dc44cfbdfa
hemg2 <hemg2@naver.com>
2023-04-17 16:02:30 +0900

    1단계 앞부분

---
+++ CodingTest/0단계 진행중/1단계.swift
@@ -0,0 +1,51 @@
+//
+//  1단계.swift
+//  CodingTest
+//
+//  Created by 1 on 2023/04/17.
+//
+
+import Foundation
+
+
+/*
+
+  문자열 내 p 와 y 의 갯수
+  func solution(_ s:String) -> Bool {
+      var countP = 0
+      var countY = 0
+      for i in s {
+  //        print("\(i)i 의값")
+          if i == "p" || i == "P" {
+              countP += 1
+          } else if i == "Y" || i == "y" {
+              countY += 1
+          }
+      }
+      print(countP)
+      print(countY)
+      if countP == countY {
+          return true
+      } else {
+          return false
+      }
+  }
+  print(solution("pPoooyY"))
+  print(solution("Pyy"))
+  
+  
+   나머지가 1이 되는 수 찾기
+  //func solution(_ n:Int) -> Int {
+  //    var a = 0
+  //    for i in 2...n {
+  //        if n % i == 1 {
+  //         return i
+  //        }
+  //    }
+  //    return a
+  //}
+  //print(solution(10))
+ 
+ 
+ 
+ */
