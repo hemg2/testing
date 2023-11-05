22f57ffbbb3884d851f652b0fc0816e00ac66bb8
hemg2 <hemg2@naver.com>
2023-11-05 23:55:30 +0900

    배열1

---
+++ CodingTest/백준 스터디/1주차 배열, 연결리스트.swift
@@ -0,0 +1,22 @@
+//
+//  1주차 배열, 연결리스트.swift
+//  CodingTest
+//
+//  Created by 1 on 11/5/23.
+//
+
+/* 배열
+ 알파벳 갯수
+ var input = readLine()!
+ var count: [Int] = Array(repeating: 0, count: 26)
+ input = input.lowercased()
+
+ for char in input {
+     if let asciiValue = char.asciiValue {
+        let index = Int(asciiValue) - 97
+         count[index] += 1
+     }
+ }
+
+ print(count.map{ String($0) }.joined(separator: " "))
+*/
