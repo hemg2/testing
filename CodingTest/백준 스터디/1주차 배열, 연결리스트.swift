da0f2f069eb833791110331a80aef459eb2cf5df
hemg2 <hemg2@naver.com>
2023-11-07 23:58:41 +0900

    배열2 숫자개수

--- CodingTest/백준 스터디/1주차 배열, 연결리스트.swift
+++ CodingTest/백준 스터디/1주차 배열, 연결리스트.swift
@@ -5,7 +5,9 @@
 //  Created by 1 on 11/5/23.
 //
 
-/* 배열
+/*
+
+ 배열
  알파벳 갯수
  var input = readLine()!
  var count: [Int] = Array(repeating: 0, count: 26)
@@ -19,4 +21,24 @@
  }
 
  print(count.map{ String($0) }.joined(separator: " "))
+ 
+ 숫자 갯수
+ var count = [Int](repeating: 0, count: 10)
+ var a = 1
+
+ for _ in 1...3 {
+     if let input = readLine(), let number = Int(input) {
+         a *= number
+     }
+ }
+ while a > 0 {
+     let b = a % 10
+     count[b] += 1
+     a /= 10
+ }
+
+ for i in 0...9 {
+     print("\(count[i])")
+ }
+ 
 */
