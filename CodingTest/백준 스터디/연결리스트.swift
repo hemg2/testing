889b0c484294386eae1778fca8c81defd904464f
hemg2 <hemg2@naver.com>
2023-11-16 23:36:06 +0900

    요세푸스

---
+++ CodingTest/백준 스터디/연결리스트.swift
@@ -0,0 +1,27 @@
+//
+//  연결리스트.swift
+//  CodingTest
+//
+//  Created by 1 on 11/16/23.
+//
+
+/*
+ 백준
+요세푸스
+ let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+ let n = input[0]
+ let k = input[1]
+ var a = [Int]()
+ var b = Array(1...n)
+ var c = 0 // 위치
+ while !b.isEmpty {
+ //    print(b)
+ //    print(c)
+      //  2 % 7
+     c = (c + k - 1) % b.count
+     let remove = b.remove(at: c)
+     
+     a.append(remove)
+ }
+  
+ */
