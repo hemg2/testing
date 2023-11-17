ea352eab3f8663b8f4575a65b92177309c054ae5
hemg2 <hemg2@naver.com>
2023-11-17 16:52:25 +0900

    풍선 터트리기

--- CodingTest/백준 스터디/연결리스트.swift
+++ CodingTest/백준 스터디/연결리스트.swift
@@ -24,4 +24,34 @@
      a.append(remove)
  }
   
+  풍선 터트리기
+  
+ let n = Int(readLine()!)!
+ var k = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+ var result = [Int]()
+ var c = 0 // 위치
+ var 풍선카운트 = Array(1...n)
+ while !풍선카운트.isEmpty {
+     let move = k[c % k.count]
+ //    print(move)
+     let b = k.remove(at: c % k.count)
+ //    print(b)
+     result.append(풍선카운트.remove(at: c % 풍선카운트.count))
+ //    print(result)
+     
+     guard !k.isEmpty else { break }
+     
+     if move > 0 {
+         c = (c + b - 1) % max(1, k.count)
+     } else {
+         c += b
+         if c < 0 {
+             c = (c % k.count + k.count) % k.count
+         }
+     }
+ }
+
+ print(result.map { String($0) }.joined(separator: " "))
+  
  */
