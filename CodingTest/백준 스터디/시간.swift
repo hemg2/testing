aabb11700fcf187a1889b398432cc4b8f3efc9fe
hemg2 <hemg2@naver.com>
2023-11-19 16:09:10 +0900

    시간 45분 깍기

---
+++ CodingTest/백준 스터디/시간.swift
@@ -0,0 +1,30 @@
+//
+//  시간.swift
+//  CodingTest
+//
+//  Created by 1 on 11/19/23.
+//
+
+import Foundation
+
+/*
+ 시간 관련
+ let nm = readLine()!.split(separator: " ").map { Int($0)! }
+ let h = nm[0]
+ let m = nm[1]
+ var result = [Int]()
+
+ var totalH = h * 60 + m
+ //let totalM = max(totalH - 45, 0)
+ totalH -= 45
+ if totalH < 0 {
+     totalH += (24 * 60)
+ }
+
+ var a = totalH / 60
+ var b = totalH % 60
+ print("\(a) \(b)")
+ //result.append(a)
+ //result.append(b)
+ //print(result.map {String($0)}.joined(separator: " "))
+ */
