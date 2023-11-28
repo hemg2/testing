45a2fe502fb43b27610c4d0cc183556f2788a5e0
hemg2 <hemg2@naver.com>
2023-11-28 23:44:14 +0900

    코테: 보석

--- CodingTest/백준 스터디/시간.swift
+++ CodingTest/백준 스터디/시간.swift
@@ -28,3 +28,47 @@ import Foundation
  //result.append(b)
  //print(result.map {String($0)}.joined(separator: " "))
  */
+
+
+/* 보석?
+ //func solution(_ gems:[String]) -> [Int] {
+ //    var result: [Int] = [Int]()
+ //    var gemSet: Set<String> = Set(gems)
+ //    var gemCount = gemSet.count
+ //    var left = 0
+ //    var right = 0
+ //    var gemDict: [String: Int] = [:]
+ //    var intMax = Int.max
+ //
+ //    while right < gems.count {
+ //        let gem = gems[right]
+ //
+ //        if let count = gemDict[gem] {
+ //            gemDict[gem] = count + 1
+ //        } else {
+ //            gemDict[gem] = 1
+ //        }
+ //
+ //        while gemDict.keys.count == gemCount {
+ //            if right - left < intMax {
+ //                intMax = right - left
+ //                result = [left + 1, right + 1]
+ //            }
+ //
+ //            let leftGem = gems[left]
+ //
+ //            if let count = gemDict[leftGem] {
+ //                gemDict[leftGem] = count - 1
+ //
+ //                if count - 1 == 0 {
+ //                    gemDict.removeValue(forKey: leftGem)
+ //                }
+ //            }
+ //            left += 1
+ //        }
+ //        right += 1
+ //    }
+ //    return result
+ //}
+ //print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+ */
