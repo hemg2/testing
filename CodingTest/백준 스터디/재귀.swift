a6ae053d41096f28d0ff031a77d6a60cd453042c
hemg2 <hemg2@naver.com>
2023-12-31 23:45:38 +0900

    재귀: 곱셈

---
+++ CodingTest/백준 스터디/재귀.swift
@@ -0,0 +1,33 @@
+//
+//  재귀.swift
+//  CodingTest
+//
+//  Created by 1 on 12/31/23.
+//
+
+import Foundation
+
+/*
+ 곱셈
+ 
+func power(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    if b == 0 {
+        return 1
+    }
+    
+    let aa = power(a, b / 2, c)
+    let bb = (aa * aa) % c
+    
+    if b % 2 == 0 {
+        return bb
+    } else {
+        return (a * bb) % c
+    }
+}
+
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+if input.count == 3{
+    let result = power(input[0], input[1], input[2])
+    print(result)
+}
+ */
