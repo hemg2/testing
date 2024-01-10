bff4af4ce794e15a2991b3b1b5aa392a56c33fa4
hemg2 <hemg2@naver.com>
2024-01-10 23:35:23 +0900

    백트래킹: N 과 M(7)

--- CodingTest/백준 스터디/재귀.swift
+++ CodingTest/백준 스터디/재귀.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 /*
+ N 과 M(7)
+ func back(n: [Int], k: Int) {
+ //    var a = [Int]()
+     var result = ""
+     var stack = [String]()
+     
+     func backtrack() {
+         if stack.count == k {
+             result += stack.joined(separator: " ") + "\n"
+             return
+         }
+         
+         for i in 0..<n.count {
+             stack.append(String(n[i]))
+             backtrack()
+             stack.removeLast()
+         }
+     }
+     backtrack()
+     print(result)
+ }
+
+ let input = readLine()!.split(separator: " ").compactMap({ Int($0) })
+ let secondInput = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
+
+ let n = input[0]
+ let k = input[1]
+ if secondInput.count == n {
+     back(n: secondInput, k: k)
+ }
+ */
+/*
  N 과 M(6)
  func back(n: [Int], k: Int) {
      var a = [Int]()
