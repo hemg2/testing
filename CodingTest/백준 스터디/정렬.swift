674de974bbd400f69ce1642b520fcfe3de6a8dfc
hemg2 <hemg2@naver.com>
2024-01-14 21:28:22 +0900

    정렬: 수 정렬하기

---
+++ CodingTest/백준 스터디/정렬.swift
@@ -0,0 +1,20 @@
+//
+//  정렬.swift
+//  CodingTest
+//
+//  Created by 1 on 1/14/24.
+//
+
+import Foundation
+
+/*
+ 수 정렬하기
+ let input = readLine()!
+ let number = Int(input)!
+ var result = [Int]()
+ for _ in 0..<number {
+     let row = Int(readLine()!)!
+     result.append(row)
+ }
+ print(result.sorted().map {String($0)}.joined(separator: "\n"))
+ */
