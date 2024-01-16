9cf0854ba02bf27e0f103767ef1dd86dc693d7b0
hemg2 <hemg2@naver.com>
2024-01-16 15:43:50 +0900

    정렬: 좌표 정렬하기(2)

--- CodingTest/백준 스터디/정렬.swift
+++ CodingTest/백준 스터디/정렬.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 /*
+ 좌표 정렬하기 2
+ let input = readLine()!
+ let number = Int(input)!
+ var result = [(Int, Int)]()
+
+ for _ in 0..<number {
+     let row = readLine()!.split(separator: " ").compactMap { Int($0) }
+     let n = row[0]
+     let m = row[1]
+     result.append((n, m))
+ }
+ result.sort {
+     if $0.1 != $1.1 {
+         return $0.1 < $1.1
+     } else {
+         return $0.0 < $1.0
+     }
+ }
+
+ for i in result {
+     print("\(i.0) \(i.1)")
+ }
+ */
+/*
  좌표 정렬하기
  let input = readLine()!
  let number = Int(input)!
