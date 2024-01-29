127b0ef635d6abbdc06fd00adf9ebe96ff284f35
hemg2 <hemg2@naver.com>
2024-01-30 00:17:12 +0900

    1로 만들기 런타임 에러 해결

--- CodingTest/백준 스터디/DP 다이나믹.swift
+++ CodingTest/백준 스터디/DP 다이나믹.swift
@@ -12,7 +12,7 @@ import Foundation
  let n = Int(readLine()!)!
  var dp = [Int](repeating: 0, count: n + 1)
 
- for i in 2...n {
+ for i in 2...n { -> for i in 2..<n+1 { 이렇게하니 런타임 에러 해결
      dp[i] = dp[i - 1] + 1
      
      if i % 2 == 0 {
