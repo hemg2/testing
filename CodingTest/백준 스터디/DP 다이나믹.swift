18c35bee8b1d1321787d76543fe7508faa80ab92
hemg2 <hemg2@naver.com>
2024-01-31 23:20:55 +0900

    DP: 1,2,3더하기

--- CodingTest/백준 스터디/DP 다이나믹.swift
+++ CodingTest/백준 스터디/DP 다이나믹.swift
@@ -6,7 +6,33 @@
 //
 
 import Foundation
+/*
+ func 더하기_1_2_3숫자(n: Int) -> Int {
+     guard n > 0 else { return 0 }
+     var dp = [Int](repeating: 0, count: n + 1)
+     dp[0] = 1
+     if n > 0 {
+         dp[1] = 1
+     }
+     if n > 1 {
+         dp[2] = 2
+         
+         for i in 3..<n+1 {
+             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+         }
+     }
+     
+     return dp[n]
+ }
+ let input = Int(readLine()!)!
+ var result = [String]()
 
+ for _ in 0..<input {
+     let n = Int(readLine()!)!
+     result.append(String(더하기_1_2_3숫자(n: n)))
+ }
+ print(result.joined(separator: "\n"))
+ */
 /*
  1로 만들기
  let n = Int(readLine()!)!
