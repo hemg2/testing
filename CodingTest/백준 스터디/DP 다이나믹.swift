710736b808ba0428e96300c8c10deb94da6f0055
hemg2 <hemg2@naver.com>
2024-02-01 09:06:00 +0900

    DP:계단오르기

--- CodingTest/백준 스터디/DP 다이나믹.swift
+++ CodingTest/백준 스터디/DP 다이나믹.swift
@@ -7,6 +7,33 @@
 
 import Foundation
 /*
+ func 계단오르기(n: [Int]) -> Int {
+     guard n.count > 0 else { return 0 }
+     if n.count == 1 { return n[0] }
+     var dp = [Int](repeating: 0, count: n.count)
+     dp[0] = n[0]
+     dp[1] = n[0] + n[1]
+     
+     if n.count > 2 {
+         dp[2] = max(n[1] + n[2], n[0] + n[2])
+     
+     
+     for i in 3..<n.count {
+         dp[i] = max(dp[i-2] + n[i], dp[i-3] + n[i-1] + n[i])
+     }
+     }
+     
+     return dp[n.count - 1]
+ }
+ let n = Int(readLine()!)!
+ var result = [Int]()
+ for i in 0..<n {
+     let m = Int(readLine()!)!
+     result.append(m)
+ }
+ print(계단오르기(n: result))
+ */
+/*
  func 더하기_1_2_3숫자(n: Int) -> Int {
      guard n > 0 else { return 0 }
      var dp = [Int](repeating: 0, count: n + 1)
