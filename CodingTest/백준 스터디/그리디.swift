0e39cd1e4ecf1c373a6f0b603bec5835887064bf
hemg2 <hemg2@naver.com>
2024-02-13 21:02:47 +0900

    그리디: 동전0

---
+++ CodingTest/백준 스터디/그리디.swift
@@ -0,0 +1,32 @@
+//
+//  그리디.swift
+//  CodingTest
+//
+//  Created by 1 on 2/13/24.
+//
+
+import Foundation
+
+/*
+ 동전 0
+ let input = readLine()!.split(separator: " ").map { Int($0)! }
+ let n = input[0]
+ var m = input[1]
+
+ var coin = [Int]()
+ for _ in 0..<n {
+     let a = Int(readLine()!)!
+     coin.append(a)
+ }
+ coin.sort(by: >)
+
+ var count = 0
+ for i in coin {
+     if m >= i {
+         count += m / i
+         m %= i
+     }
+ }
+
+ print(count)
+ */
