c66b193daa773b9d165c5d24a0dc9d1159d305d5
hemg2 <hemg2@naver.com>
2024-02-18 22:48:39 +0900

    그리디: 동전0

--- CodingTest/백준 스터디/그리디.swift
+++ CodingTest/백준 스터디/그리디.swift
@@ -6,7 +6,23 @@
 //
 
 import Foundation
+/*
+ 로프
+ let n = Int(readLine()!)!
+ var ro = [Int]()
+ for _ in 0..<n {
+     let a = Int(readLine()!)!
+     ro.append(a)
+ }
+ ro.sort(by: >)
 
+ var maxs = 0
+ for (index, rope) in ro.enumerated() {
+     let a = rope * (index + 1)
+     maxs = max(maxs, rope * (index + 1))
+ }
+ print(maxs
+ */
 /*
  동전 0
  let input = readLine()!.split(separator: " ").map { Int($0)! }
