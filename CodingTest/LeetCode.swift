7b90836b52c5c295324bfc29a98d6cead173f71e
hemg2 <hemg2@naver.com>
2024-03-31 23:15:47 +0900

    removeElement

---
+++ CodingTest/LeetCode.swift
@@ -0,0 +1,36 @@
+//
+//  LeetCode.swift
+//  CodingTest
+//
+//  Created by Hemg on 3/31/24.
+//
+
+import Foundation
+
+
+/*
+ 스트링 1번
+ //func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+ //    var result = 0
+ //
+ //    for i in 0..<nums.count {
+ //        if nums[i] != val {
+ //            nums[result] = nums[i]
+ //            result += 1
+ //        }
+ //    }
+ //    return result
+ //}
+
+ //func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+ //    for i in 0..<n {
+ //        nums1[m + i] = nums2[i]
+ //        print("\(nums1)--\(m)m \(i)i")
+ //    }
+ //    nums1.sort()
+ //}
+ //
+ //var number = [1, 2, 3, 0, 0, 0]
+ //merge(&number, 3, [2, 5, 6], 3)
+ //print(number)
+ */
